29c95a01810b416c925049367d8a8be4512efe29
Vitaliy <bering856@gmail.com>
2025-03-24 21:52:32 +0200

    init commit

---
+++ electron.mjs
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+  });
+
+  // win.webContents.openDevTools();
+
+  if (app.isPackaged) {
+    win.loadFile(path.join(__dirname, "dist/index.html"));
+  } else {
+    win.loadURL("http://localhost:3000");
+  }
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") app.quit();
+});
